81e0153d09f8e3a22917f36291c54d77c3efd194
lhs <fantasytotal@naver.com>
2024-12-12 16:59:13 +0900

    [feat] 월드컵 정보 업데이트 기능 구현
    월드컵 저장 시 알림 추가
    몇몇 클래스명 소문자인 것 수정

--- src/main/java/com/metacoding/projectwc/worldcup/WorldcupController.java
+++ src/main/java/com/metacoding/projectwc/worldcup/WorldcupController.java
@@ -2,11 +2,9 @@ package com.metacoding.projectwc.worldcup;
 
 import com.metacoding.projectwc._core.util.Resp;
 import com.metacoding.projectwc.user.User;
-import com.metacoding.projectwc.worldcup.item.WorldcupItemRequest;
 import com.metacoding.projectwc.worldcup.item.WorldcupItemService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -36,15 +34,15 @@ public class WorldcupController {
         // TODO 유저의 월드컵 id가 맞는지 체크
         // User seesionUser = (User) session.getAttribute("sessionUser");
         // worldcupService.findById(id).getUser() 같은지 확인
-        worldcupService.findById(id);
-        model.addAttribute("id", id);
+        WorldcupResponse.FindByIDForWcFormDTO findByIDForWcFormDTO = worldcupService.findByIdForWcForm(id);
+        model.addAttribute("model", findByIDForWcFormDTO);
         return "wc-form";
     }
 
     @GetMapping("/worldcups/start-form/{id}")
     public String startForm(@PathVariable("id") int id, Model model) {
         List<Integer> round = worldcupItemService.countAll(id);
-        WorldcupResponse.findByIDDTO worldcup = worldcupService.findById(id);
+        WorldcupResponse.FindByIDDTO worldcup = worldcupService.findById(id);
         model.addAttribute("allItem", round.get(0));
         model.addAttribute("round", round);
         model.addAttribute("id", id);
@@ -60,34 +58,40 @@ public class WorldcupController {
     }
 
     @GetMapping("/main")
-    public String main(Model model, WorldcupRequest.findAllDTO findAllDTO) {
+    public String main(Model model, WorldcupRequest.FindAllDTO findAllDTO) {
 
         // findAllDTO 디폴트 값은 1페이지, 사이즈는 10, 최신순
-        List<WorldcupResponse.findAllDTO> worldcupList = worldcupService.findAllByTiltle(findAllDTO);
+        List<WorldcupResponse.FindAllDTO> worldcupList = worldcupService.findAllByTiltle(findAllDTO);
         model.addAttribute("worldcupList", worldcupList);
 
         // 페이지 정보
-        WorldcupResponse.pageDTO pageDTO = worldcupService.createPageDTO(findAllDTO);
+        WorldcupResponse.PageDTO pageDTO = worldcupService.createPageDTO(findAllDTO);
         model.addAttribute("pageDTO", pageDTO);
 
         return "main";
     }
 
-    @GetMapping("/mine")
-    public String mine(Model model, WorldcupRequest.findAllDTO findAllDTO) {
+    @GetMapping("/s/worldcups/mine")
+    public String mine(Model model, WorldcupRequest.FindAllDTO findAllDTO) {
         // TODO 로그인 기능 완성될 경우 적용
 //        User seesionUser = (User) session.getAttribute("sessionUser");
 
         User user = User.builder().id(1).build();
 
         // 로그인 한 유저가 생성한 월드컵 정보만 가져온다.
-        List<WorldcupResponse.findAllDTO> worldcupList = worldcupService.findAllByTiltleAndUser(findAllDTO, user);
+        List<WorldcupResponse.FindAllDTO> worldcupList = worldcupService.findAllByTiltleAndUser(findAllDTO, user);
         model.addAttribute("worldcupList", worldcupList);
 
         // 페이지 정보
-        WorldcupResponse.pageDTO pageDTO = worldcupService.createPageDTOForMine(findAllDTO, user);
+        WorldcupResponse.PageDTO pageDTO = worldcupService.createPageDTOForMine(findAllDTO, user);
         model.addAttribute("pageDTO", pageDTO);
 
         return "mine";
+    }
+
+    @PutMapping("/worldcups/{id}")
+    public ResponseEntity<?> update(@PathVariable int id, @RequestBody WorldcupRequest.UpdateDTO updateDTO) {
+        worldcupService.update(id, updateDTO);
+        return ResponseEntity.ok(Resp.ok("갱신됨"));
     }
 }
--- src/main/java/com/metacoding/projectwc/worldcup/WorldcupRequest.java
+++ src/main/java/com/metacoding/projectwc/worldcup/WorldcupRequest.java
@@ -5,11 +5,17 @@ import lombok.Data;
 public class WorldcupRequest {
 
     @Data
-    public static class findAllDTO {
+    public static class FindAllDTO {
         private String searchKeyword = ""; // 검색 단어
         private String sortBy = "Latest"; // 정렬 기준, Latest, Popularity
         private Integer page = 1; // 현재 페이지 번호
         private Integer size = 10; // 페이지 크기 (한 페이지당 아이템 개수)
     }
 
+    @Data
+    public static class UpdateDTO {
+        private String title;
+        private String description;
+        private int visibility;
+    }
 }
